d890de7830d1cf32db6d2eb09d80f8e826ea07c2
lookwendyican <wendyalers@gmail.com>
2019-07-05 00:48:15 -0400

    reworked js file on DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,10 +5,9 @@
 
 
   // Creating variables to hold the number of wins, losses, and ties. They start at 0.
-  var winsCount = 0;
-  var lossesCount = 0;
-  var guessesLeft = 10;
-  var guessedSoFar = [];
+  var roundCount = 0;
+  var computerCount = 0;
+  var userCount = 0;
  
  
  // Create variables that hold references to the places in the HTML where we want to display things.
@@ -17,16 +16,11 @@
 
 var  roundNumberDisplay = document.getElementById('round-number');
 var  targetLetterDisplay = document.getElementById('target-letter');
-var  computerScoreDisplay = document.getElementById('computer-score');
-var  humanScoreDisplay = document.getElementById('human-score');
-var  humanGuessInput = document.getElementById('human-guess');
 var  computerGuessDisplay = document.getElementById('computer-guess');
-
-
-
-var  computerWinsDisplay = document.getElementById('computer-wins');
-
-var  guessButton = document.getElementById('guess');
+var  computerScoreDisplay = document.getElementById('computer-score');
+var  userGuessDisplay = document.getElementById('user-guess');
+var  userScoreDisplay = document.getElementById('user-score');
+var  winnerDisplay = document.getElementById('winner-display');
 var  nextRoundButton = document.getElementById('next-round')
 
 
@@ -34,18 +28,35 @@ var  nextRoundButton = document.getElementById('next-round')
 // To start Game, Prompt use to press letter
 //ALLERT
 
-// set up the computer (PSYCHIC)  to pick a random indexed value from aray
-var randomIndex = Math.floor(Math.random() * letters.length);
-var computerChoice = letters[randomIndex];
+document.onkeyup = function (event) {
+    
+var userChoice = event.key;
+  // make sure the user selects a value a-z
+  var regexp = /[a-z]/gi;
+        if (!regexp.test(userChoice)) {
+          alert("Please enter a letter from A to Z!");
+        }  else {
+          console.log(userChoice);
+        }
+}
+// set up the computer (PSYCHIC)  to pick a random indexed value from array
 
-console.log(computerChoice);
+var randomIndexPsychic = Math.floor(Math.random() * letters.length);
+var targetLetterDisplay = letters[randomIndexPsychic];
+console.log(targetLetterDisplay);
+
 
 
 //HAVE COMPUTER choose another letter to DISPLAY separately from PSYCHIC letter.
 
-
+var randomIndexCompGuess = Math.floor(Math.random() * letters.length);
+var computerGuessDisplay = letters[randomIndexCompGuess];
+console.log(computerGuessDisplay);
 
 //Log user and computer guesses to the SPACE PROVIDED
+
+
+
 
 
 //AFTER... display PSYCHIC guess
@@ -59,7 +70,4 @@ console.log(computerChoice);
 
 
  //Activate Next round Button and reset guess ???
-
-
-
 
